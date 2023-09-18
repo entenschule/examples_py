5bf324c4375af65cbf94fcf59b4a720f0e9b2dcb
Entenschule <entenschule@users.noreply.github.com>
2023-09-18 13:30:06 +0200

    details

--- a001_misc/b007_ascii_codepages/__main__.py
+++ a001_misc/b007_ascii_codepages/__main__.py
@@ -12,17 +12,17 @@
 
 
 # The content of this file will be the two bytes C2 A5.
-with open('ambiguous.txt', 'wb') as f:
+with open('AMBIGUOUS.txt', 'wb') as f:
     encoded = "┬Ц".encode('cp855')  # codepage 855 (cyrillic)
     assert encoded == b'\xc2\xa5'
     f.write(encoded)
 
-with open('ambiguous.txt', 'rb') as f:
+with open('AMBIGUOUS.txt', 'rb') as f:
     encoded = f.read(2)
     decoded = encoded.decode('cp857')  # codepage 857 (turkish)
     assert decoded == '┬Ñ'
 
-with open('ambiguous.txt', 'r') as f:
+with open('AMBIGUOUS.txt', 'r') as f:
     assert f.read(1) == '¥'
 
 
@@ -30,14 +30,26 @@ with open('ambiguous.txt', 'r') as f:
 
 
 # The content of this file will also be the two bytes C2 A5.
-with open('yen_sign.txt', 'w') as f:
+with open('yen.txt', 'w') as f:
     s = '¥'
     assert hex(ord(s)) == '0xa5'
     f.write(s)
 
 
 # The content of this file will be the three bytes EC 8A A5.
-with open('hangul_syllable_seug.txt', 'w') as f:
+with open('seug.txt', 'w') as f:
     s = '슥'
     assert hex(ord(s)) == '0xc2a5'
+    f.write(s)
+
+
+# The content of this file will be the byte 61.
+with open('a.txt', 'w') as f:
+    s = 'a'
+    assert hex(ord(s)) == '0x61'
+    f.write(s)
+
+# The content of this file will be the six bytes 61 C2 A5 EC 8A A5.
+with open('a_yen_seug.txt', 'w') as f:
+    s = 'a¥슥'
     f.write(s)
