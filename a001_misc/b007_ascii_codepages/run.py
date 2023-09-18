3c07d1fc427818d1abdfefe7950d7234c78aeaaa
Entenschule <entenschule@users.noreply.github.com>
2023-09-18 12:22:58 +0200

    extend encoding example

--- a001_misc/b007_ascii_codepages/run.py
+++ a001_misc/b007_ascii_codepages/run.py
@@ -6,8 +6,12 @@
 Ñ    LATIN CAPITAL LETTER N WITH TILDE
 
 ¥    YEN SIGN
+
+슥    Hangul Syllable Seug
 """
 
+
+# The content of this file will be the two bytes C2 AF.
 with open('ambiguous.txt', 'wb') as f:
     encoded = "┬Ц".encode('cp855')
     assert encoded == b'\xc2\xa5'
@@ -20,3 +24,20 @@ with open('ambiguous.txt', 'rb') as f:
 
 with open('ambiguous.txt', 'r') as f:
     assert f.read(1) == '¥'
+
+
+###################################################################
+
+
+# The content of this file will also be the two bytes C2 AF.
+with open('yen_sign.txt', 'w') as f:
+    s = '¥'
+    assert hex(ord(s)) == '0xa5'
+    f.write(s)
+
+
+# The content of this file will also be the three bytes EC 8A A5.
+with open('hangul_syllable_seug.txt', 'w') as f:
+    s = '슥'
+    assert hex(ord(s)) == '0xc2a5'
+    f.write(s)
